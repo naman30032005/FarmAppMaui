13dfa0992d9a184f3f69da4d32517266d8b2351d
Thomas <chhunthireach23@gmail.com>
2025-10-22 09:47:01 +1300

    test TotalAnimal

--- ViewModels/DashboardVM.cs
+++ ViewModels/DashboardVM.cs
@@ -1,11 +1,29 @@
-﻿
-namespace Farm.ViewModels;
+﻿namespace Farm.ViewModels;
 
-public partial class DashboardVM
+// Implementation part for the partial property
+public partial class DashboardVM : ObservableObject
 {
+    private readonly DbOps _dbOps;
+
+    [ObservableProperty]
+    private int totalAnimal;
+
+    public DashboardVM()
+    {
+        _dbOps = new DbOps();
+        LoadTotalsAnimal();
+    }
+
+    [RelayCommand]
+    public async Task LoadTotalsAnimal()
+    {
+        totalAnimal = await _dbOps.GetTotalAnimalsAsync();
+    }
+
     [RelayCommand]
     public async Task ForecastPage()
     {
         await Shell.Current.GoToAsync($"{nameof(Forecast)}", true);
     }
 }
+
