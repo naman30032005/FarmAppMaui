5ae4e24ede98f7d834a7ac55ac21d7df322371b6
Thomas <chhunthireach23@gmail.com>
2025-11-04 09:31:15 +1300

    Add DashboardVM

--- ViewModels/DashboardVM.cs
+++ ViewModels/DashboardVM.cs
@@ -1,29 +1,65 @@
 ﻿namespace Farm.ViewModels;
 
-// Implementation part for the partial property
-public partial class DashboardVM : ObservableObject
+public partial class DashboardVM : BaseVM
 {
-    private readonly DbOps _dbOps;
+    private readonly DbOps _db;
+    public List<AnimalVM> Animals = new List<AnimalVM>();
 
-    [ObservableProperty]
-    private int totalAnimal;
-
-    public DashboardVM()
+    public DashboardVM(DbOps dbs)
     {
-        _dbOps = new DbOps();
-        LoadTotalsAnimal();
+        _db = dbs;
     }
 
-    [RelayCommand]
-    public async Task LoadTotalsAnimal()
+
+    [ObservableProperty] private int totalAnimal;
+    [ObservableProperty] private int totalCow;
+    [ObservableProperty] private int totalSheep;
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(ProfitorLoss))] private float prol;
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(CowProfitDisplay)), NotifyPropertyChangedFor(nameof(SheepProfitDisplay))] private float cowProfit;
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(SheepProfitDisplay)), NotifyPropertyChangedFor(nameof(CowProfitDisplay))] private float sheepProfit;
+
+    public string CowProfitDisplay => $"Cow: {CowProfit:F1}$ ({(CowProfit > SheepProfit ? "More" : "Less")} Profitable)";
+    public string SheepProfitDisplay => $"Sheep: {SheepProfit:F1}$ ({(SheepProfit > CowProfit ? "More" : "Less")} Profitable)";
+
+    public string ProfitorLoss => (prol > 0) ? "Daily Profit " : "Daily Loss ";
+
+    public async Task CalculateField()
     {
-        totalAnimal = await _dbOps.GetTotalAnimalsAsync();
+        if (Animals.Count == 0) await ReadData();
+        else
+        {
+            Animals.Clear();
+            await ReadData();
+        }
+        var prol = Calculator.IncomePerDay(Animals) - Calculator.ExpensePerDay(Animals);
+        var avg = Animals.Average(x => x.Weight);
+        var cowCount = Animals.Count(a => a.AnimalType == nameof(Cow));
+        var sheepCount = Animals.Count(a => a.AnimalType == nameof(Sheep));
+        var cp = (Calculator.IncomePerDay(Animals.Where(x => x.AnimalType == nameof(Cow)).ToList()) - Calculator.ExpensePerDay(Animals.Where(x => x.AnimalType == nameof(Cow)).ToList())) / Animals.Where(x => x.AnimalType == nameof(Cow)).Count();
+        var sp = (Calculator.IncomePerDay(Animals.Where(x => x.AnimalType == nameof(Sheep)).ToList()) - Calculator.ExpensePerDay(Animals.Where(x => x.AnimalType == nameof(Sheep)).ToList())) / Animals.Where(x => x.AnimalType == nameof(Sheep)).Count(); ;
+
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            Prol = (float)prol;
+            TotalAnimal = Animals.Count;
+            CowProfit = cp;
+            SheepProfit = sp;
+            TotalCow = cowCount;
+            TotalSheep = sheepCount;
+        });
     }
 
-    [RelayCommand]
-    public async Task ForecastPage()
+    async Task ReadData()
     {
-        await Shell.Current.GoToAsync($"{nameof(Forecast)}", true);
+        IsBusy = true;
+
+        Animals.Clear();
+
+        var animalList = await _db.ReadDataAsync();
+        foreach (var animal in animalList)
+            Animals.Add(new AnimalVM(animal));
+
+        IsBusy = false;
     }
 }
 
